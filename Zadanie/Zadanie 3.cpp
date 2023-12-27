db9700a5b96802be7155ddd8186de2e9def4402a
emkurilkoryumin <146382095+emkurilkoryumin@users.noreply.github.com>
2023-12-27 15:36:28 +0300

    Update Zadanie 3.cpp

--- Zadanie/Zadanie 3.cpp
+++ Zadanie/Zadanie 3.cpp
@@ -2,49 +2,42 @@
 #include <iostream>
 using namespace std;
 
-class MedianFinder {
-private:
+void addNum(priority_queue<int>& maxHeap, priority_queue<int, vector<int>, greater<int>>& minHeap, int num) {
+    if (maxHeap.empty() || num < maxHeap.top()) {
+        maxHeap.push(num);
+    }
+    else {
+        minHeap.push(num);
+    }
+
+    if (maxHeap.size() > minHeap.size() + 1) {
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+    }
+    else if (minHeap.size() > maxHeap.size()) {
+        maxHeap.push(minHeap.top());
+        minHeap.pop();
+    }
+}
+
+double findMedian(priority_queue<int>& maxHeap, priority_queue<int, vector<int>, greater<int>>& minHeap) {
+    if (maxHeap.size() == minHeap.size()) {
+        return (maxHeap.top() + minHeap.top()) / 2.0;
+    }
+    else {
+        return maxHeap.top();
+    }
+}
+
+int main() {
     priority_queue<int> maxHeap;
     priority_queue<int, vector<int>, greater<int>> minHeap;
 
-public:
-    MedianFinder() {}
+    addNum(maxHeap, minHeap, 2);
+    addNum(maxHeap, minHeap, 3);
+    cout << "Median: " << findMedian(maxHeap, minHeap) << endl;
+    addNum(maxHeap, minHeap, 4);
+    cout << "Median: " << findMedian(maxHeap, minHeap) << endl;
 
-        void addNum(int num) {
-        if (maxHeap.empty() || num < maxHeap.top()) {
-            maxHeap.push(num);
-        }
-        else {
-            minHeap.push(num);
-        }
-
-        if (maxHeap.size() > minHeap.size() + 1) {
-            minHeap.push(maxHeap.top());
-            maxHeap.pop();
-        }
-        else if (minHeap.size() > maxHeap.size()) {
-            maxHeap.push(minHeap.top());
-            minHeap.pop();
-        }
-    }
-
-    double findMedian() {
-        if (maxHeap.size() == minHeap.size()) {
-            return (maxHeap.top() + minHeap.top()) / 2.0;
-        }
-        else {
-            return maxHeap.top();
-        }
-    }
-};
-
-int main() {
-    MedianFinder mf;
-    mf.addNum(2);
-    mf.addNum(3);
-    cout << "Median: " << mf.findMedian() << endl;
-    mf.addNum(4);
-    cout << "Median: " << mf.findMedian() << endl;
-
-        return 0;
+    return 0;
 }
